61ed12837e7eb0dd8dab6c16325fb0ee1cce37c7
Ivan <ivanvt1095@gmail.com>
2024-05-14 11:26:14 -0300

    commit temerario

--- JavaPrueba.js
+++ JavaPrueba.js
@@ -1,48 +1,16 @@
-function sumarDos(num1,num2){
-    var suma = num1 + num2;
-    return suma;
+var perro = {
+
+    nombre: "milo",
+    edad: 12,
+    vivo: false,
+    quienSoy(){return "soy " + this.nombre},
+    ladrar(){return this.nombre+" dice guau"}
 }
-n1=2;
-n2=3;
-var resultado = sumarDos(n1,n2);
-console.log("El resultado es: " + resultado);
-
-var suma = function sumarTres(numero1,numero2) {
-    return numero1+numero2;
+document.write(perro.nombre+" tiene "+ perro.edad+" anios de edad.")
+document.write(perro.quienSoy())
+if(perro.vivo == true) {
+    document.write(perro.ladrar())
+} else {
+    document.write("el perro ta morido")
 }
-console.log(suma(40,15))
 
-
-function cuadrado(x){
-    return x*x
-}
-console.log(cuadrado(2))
-
-var aCuadrado = x => x/x+x;
-document.write(aCuadrado(3));
-
-function sumar(num1,num2){
-    return num1+num2
-}
-console.log(sumar(4,6))
-
-
-function multiplicar(num1,num2) {
-    producto = num1*num2
-    return producto
-}
-console.log(multiplicar(5,3))
-
-var aSumar = (num1,num2) => num1+num2;
-console.log(aSumar(5,7))
-
-var aMultiplicar =(num1,num2) =>
-    {
-        producto=num1*num2
-        return producto
-    }
-console.log(aMultiplicar(6,7));
-
-(a) => {return a +100}
-(a) => a+100;
-a => a +100;
